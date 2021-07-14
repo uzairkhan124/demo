a2a697d8484e3025d838867e1ebaaa21f25615e4
uzairkhan124 <uzairkhan1406@gmail.com>
2021-07-14 02:46:38 -0700

    homes1 commit

--- api/models/user.js
+++ api/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require ('mongoose') ;
 
-const Schema = mongoose.Schema({
+const userSchema = mongoose.Schema({
     _id  : mongoose.Schema.Types.ObjectId ,
     email : {type : String , required : true},
     password : {type : String , required : true}
 });
 
-module.exports = mongoose.model('User', productSchema);
+module.exports = mongoose.model('User', userSchema);
